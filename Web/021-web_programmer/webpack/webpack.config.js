21cd6268c3363a60e2830d5b5a93558dce25bd93
sunshine <sun412926@gmail.com>
2022-02-13 15:47:24 +0800

    :rocket: init

--- Web/021-web_programmer/webpack/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge');
-const baseConfig = require('./webpack.base.config');
-const reactConfig = require('./webpack.react.config');
-const lessConfig = require('./webpack.less.config');
-
-const config = merge(baseConfig, reactConfig, lessConfig);
-
-module.exports = config;
